4e572477515d136920c07c00ff737d5e22a561df
Varun <56618424+VarunSri0902@users.noreply.github.com>
2021-09-24 21:59:40 -0700

    Placeholder Home page

--- Varun/Pages/home.dart
+++ Varun/Pages/home.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:test_app1/shared/constants.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[100],
+      appBar: appBar,
+      body: Container(
+        width: 200.0,
+        height: 100.0,
+        color: Colors.blueAccent[400],
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+        child: Text(
+          'Home Page',
+          style: TextStyle(fontSize: 50.0),
+        ),
+      ),
+    );
   }
 }
